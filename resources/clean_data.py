4f4ca0d4c7aafbc18d40a5a375261e7166f2ee64
RSDC6991 <amathur018@gmail.com>
2019-06-18 00:30:10 +0530

    Update clean_data.py

--- resources/clean_data.py
+++ resources/clean_data.py
@@ -16,6 +16,16 @@ case_study_data.info()
 print('data head')
 print(case_study_data.head())
 
+print('dataset - promotion')
+promotion = pd.read_csv('../data_orig/promotion.csv')
+promotion.rename(columns={
+        'FU': 'SKU',
+        'Weeks': 'ISO_Week'
+        }, inplace=True)
+promotion['Promotion'] = 1
+print(promotion.head())
+print(promotion.describe())
+
 
 def plotting(dat):
     import matplotlib.pyplot as plt
@@ -30,18 +40,22 @@ print('\n\ndescriptive statistics\n')
 uniqueColNames = case_study_data.SKU.unique()
 for colName in uniqueColNames:
     print('\n\n\t\t Item : {0}'.format(colName))
+    # obtain unique rows for that particular consumer good
     uniqueData = case_study_data[case_study_data['SKU'] == colName]
+    # filling all NaN's with mean values
     uniqueData.fillna(uniqueData.mean(), inplace=True)
 
+    # plotting to get better idea
     plotting(uniqueData)
+    # obtaining mean and Standard Deviation of Sales data
     print("Column 'Sales' Mean {0}\nSTD : {1}".format(
             uniqueData['Sales'].mean(), uniqueData['Sales'].std()))
 
-promotion = pd.read_csv('../data_orig/promotion.csv')
-print(promotion.head())
-print(promotion.describe())
+    # merging dataframe with promotion dataframe as a left join
+    # and then replace all NaN's with default value 0, denoting not a promotion
+    mergedDataframe = pd.merge(uniqueData, promotion, on=['SKU', 'ISO_Week'],
+                               how='left')
+    mergedDataframe.fillna(0, inplace=True)
 
-
-
-
-
+    # showcase the data frame
+    print(mergedDataframe)
