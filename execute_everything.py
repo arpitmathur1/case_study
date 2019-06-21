aa2aa3934dc10e9d70551a79cb02657d563e9e14
RSDC6991 <amathur018@gmail.com>
2019-06-22 01:36:04 +0530

    Update execute_everything.py

--- execute_everything.py
+++ execute_everything.py
@@ -1,8 +1,16 @@
-
+# AUTHOR : Arpit Mathur
+# Script Creation date: 22-06-2019 1:35AM
 # execute everything
 
 try:
+    # this shall execute the entirity of the cleaning data script
+    # since it doesn't contain any class / methods
     import resources.clean_data
 except Exception as ex:
     print(ex)
 
+try:
+    # this shall execute the entirity of the creation of the model script
+    import resources.create_final_prediction_model
+except Exception as ex:
+    print(ex)
