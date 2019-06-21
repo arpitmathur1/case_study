edf10a79059c894085d0d005c36e267d7a6ab161
RSDC6991 <amathur018@gmail.com>
2019-06-22 00:02:33 +0530

    model - initial SVR developed

--- resources/create_prediction_model.py
+++ resources/create_prediction_model.py
@@ -1,46 +1,79 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.svm import SVR
+import pickle
+from scipy.stats.stats import pearsonr
+from sklearn.metrics import mean_squared_error
 
 # Import data
 df = pd.read_csv('../data_clean/SKU_101.csv')
 
+# dropping rows where sales value is zer (acc. to documentation provided)
+df = df[df.Sales != 0]
+
+# generate the 'future values' column
 futureValue = df['Sales'].values.tolist()[1:]
 futureValue.append(0)
 df.insert(3, "future Sales", futureValue, True)
 del(futureValue)
 
-
+# dropping unnecessary columns
 df.drop(['SKU', 'ISO_Week'], axis=1, inplace=True)
-print(df.head())
+
+# creating train-test split
 threshold = int(len(df)*0.85)
 train = df[:threshold]
 test = df[threshold:]
 del(df)
 del(threshold)
 
+# generating the X, Y dataframe
 trainY = train['future Sales']
 testY = test['future Sales']
-
 trainX = train.drop(['future Sales'], axis=1)
 testX = test.drop(['future Sales'], axis=1)
 del(train)
 del(test)
 
-print(trainX.head())
-print(trainY.head())
+# print(trainX.head())
+# print(trainY.head())
 
+# ########## Generating SVR Model ##########
 regressor = SVR(kernel='rbf', verbose=True, gamma='auto')
 regressor.fit(trainX, trainY)
-# print(regressor)
+
+filename = '../models/initial_SVR_model.savefile'
+pickle.dump(regressor, open(filename, 'wb'))
 
 predictions = regressor.predict(testX)
 # print(predictions)
 # print(testY)
 
+pearson_correlationValues = pearsonr(predictions, testY)
+print("\ncorrelation = " + str(pearson_correlationValues[0]))
+print("significance = " + str(pearson_correlationValues[1]))
+print("MSE: " + str(mean_squared_error(predictions, testY)))
+
+
 plt.plot(predictions)
 plt.plot(testY)
+plt.xlabel('compare predictions')
+plt.ylabel('sales value (scaled)')
+plt.title('SVR trial One')
 plt.show()
+
+
+# #################3 Generate Random Forest Regressor Model ###
+
+
+
+
+
+
+
+
+
+
 
 
 
