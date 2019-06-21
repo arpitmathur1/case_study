432ba7a069703fb1a93948b32b553fa6d02020ad
RSDC6991 <amathur018@gmail.com>
2019-06-21 23:46:45 +0530

    version one - basic model created

--- resources/create_prediction_model.py
+++ resources/create_prediction_model.py
@@ -1,22 +1,46 @@
 import pandas as pd
-from statsmodels.graphics.tsaplots import plot_acf
 import matplotlib.pyplot as plt
-from statsmodels.tsa.arima_model import ARIMA
-
+from sklearn.svm import SVR
 
 # Import data
 df = pd.read_csv('../data_clean/SKU_101.csv')
 
-# 1,1,2 ARIMA Model
-model = ARIMA(df.Sales, order=(1, 1, 2))
-model_fit = model.fit(disp=0)
-print(model_fit.summary())
+futureValue = df['Sales'].values.tolist()[1:]
+futureValue.append(0)
+df.insert(3, "future Sales", futureValue, True)
+del(futureValue)
 
-residuals = pd.DataFrame(model_fit.resid)
-fig, ax = plt.subplots(1, 2)
-residuals.plot(title="Residuals", ax=ax[0])
-residuals.plot(kind='kde', title='Density', ax=ax[1])
+
+df.drop(['SKU', 'ISO_Week'], axis=1, inplace=True)
+print(df.head())
+threshold = int(len(df)*0.85)
+train = df[:threshold]
+test = df[threshold:]
+del(df)
+del(threshold)
+
+trainY = train['future Sales']
+testY = test['future Sales']
+
+trainX = train.drop(['future Sales'], axis=1)
+testX = test.drop(['future Sales'], axis=1)
+del(train)
+del(test)
+
+print(trainX.head())
+print(trainY.head())
+
+regressor = SVR(kernel='rbf', verbose=True, gamma='auto')
+regressor.fit(trainX, trainY)
+# print(regressor)
+
+predictions = regressor.predict(testX)
+# print(predictions)
+# print(testY)
+
+plt.plot(predictions)
+plt.plot(testY)
 plt.show()
 
-model_fit.plot_predict(dynamic=False)
-plt.show()
+
+
